e38a5643c1d5684b7b42dd42d1d3c09df98b0b16
KimiAstom <1720964616@qq.com>
2021-05-19 00:56:36 +0800

    AA

--- src/views/account/entry.js
+++ src/views/account/entry.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './index.vue'
 import axios from 'axios'
-
+// AA
 axios.defaults.baseURL = 'http://localhost:3000'
 Vue.config.productionTip = false
 Vue.prototype.axios = axios
